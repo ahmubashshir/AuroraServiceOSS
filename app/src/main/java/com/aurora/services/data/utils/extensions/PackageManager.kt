70769c2117cea29bfd76b8c4347197394918cf71
Mubashshir <ahmubashshir@gmail.com>
2023-02-14 22:41:26 +0600

    DeBloat AuroraService & Allow only AuroraOSS
    Signed-off-by: Mubashshir <ahmubashshir@gmail.com>

--- app/src/main/java/com/aurora/services/data/utils/extensions/PackageManager.kt
+++
@@ -1,7 +0,0 @@
-package com.aurora.services.data.utils.extensions
-
-import android.content.pm.PackageManager
-
-fun PackageManager.isGranted(packageName: String, permission: String): Boolean {
-    return checkPermission(permission, packageName) == PackageManager.PERMISSION_GRANTED
-}
